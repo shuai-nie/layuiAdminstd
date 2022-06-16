80e803801698adb4f7b41c0f7a3f22ca18d2a382
nie <yesheng35@126.com>
2022-06-16 21:38:17 +0800

    add jwt add imgUpload

---
+++ ginWeb/controller/Upload.go
@@ -0,0 +1,43 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type UploadController struct {}
+
+var UploadControl = &UploadController{}
+
+func (*UploadController) UploadImage(c *gin.Context) {
+	dir := Config.StaticUrl // 配置文件中的静态资源路径
+	file, _ := c.FormFile("file")
+	filename := RandStr(40) + ".jpg"
+	//将文件保存到本地服务器的指定位置
+	if err := c.SaveUploadedFile(file, dir+filename); err != nil {
+		ResponseError(c, "文件上传失败")
+	} else {
+		ResponseSuccess(c, filename)
+	}
+}
+// 某些文件的执行依赖于其他文件，比如3D模型，所以需要通过ZIP的方式上传，同时要指定执行文件的名称
+// 先将ZIP保存，然后解压，最后删除保存的ZIP
+func (*UploadController) UploadZipFile(c *gin.Context) {
+	dir := Config.StaticUrl
+	file, _ := c.FormFile("file")
+	name := c.PostForm("name") // 执行文件名称
+	filename := RandStr(40)
+	//将文件保存到本地服务器的指定位置
+	if err := c.SaveUploadedFile(file, dir+filename+".zip"); err != nil {
+		ResponseError(c, "文件上传失败")
+	} else {
+		det, err := Unzip(filename+".zip", filename, name) // 解压
+		if err != nil {
+			ResponseError(c, "文件上传失败")
+			return
+		}
+		UploadControl.RemoveFile(filename + ".zip") // 删除zip
+		if det == "" {
+			ResponseError(c, "文件名称错误")
+		} else {
+			ResponseSuccess(c, det)
+		}
+	}
+}
--- ginWeb/main.go
+++ ginWeb/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
 func main() {
 	// 创建一个不包含任何中间件的engine
@@ -37,4 +42,106 @@ func AuthRequired() gin.HandlerFunc {
 		"lena" : "hell2",
 		"menu" : "4321",
 	})
+}
+
+const (
+	ErrorReason_ServerBusy = "服务器繁忙"
+	ErrorReason_Relogin "请重新登录"
+)
+
+func sayHello(c *gin.Context) {
+	strToken := c.Param("token")
+	claim, err := verifyAction(strToken)
+	if err != nil {
+		c.String(http.StatusNotFound, err.Error())
+		return
+	}
+	c.String(http.StatusOK, "hello", claim.Username)
+}
+
+type JWTClaims struct {
+	jwt.StandardClaims
+	UserID      int      `json:"user_id"`
+	Password    string   `json:"password"`
+	Username    string   `json:"username"`
+	FullName    string   `json:"full_name"`
+	Permissions []string `json:"permissions"`
+}
+
+var (
+	Secret     = "dong_tech" // 加盐
+	ExpireTime = 3600        // token有效期
+)
+
+func login(c *gin.Context) {
+	username := c.Param("username")
+	password := c.Param("password")
+	claims := &JWTClaims{
+		UserID:      1,
+		Username:    username,
+		Password:    password,
+		FullName:    username,
+		Permissions: []string{},
+	}
+	claims.IssuedAt = time.Now().Unix()
+	claims.ExpiresAt = time.Now().Add(time.Second * time.Duration(ExpireTime)).Unix()
+	signedToken,err:=getToken(claims)
+	if err!=nil{
+		c.String(http.StatusNotFound, err.Error())
+		return
+	}
+	c.String(http.StatusOK, signedToken)
+}
+
+func verify(c *gin.Context) {
+	strToken := c.Param("token")
+	claim,err := verifyAction(strToken)
+	if err != nil {
+		c.String(http.StatusNotFound, err.Error())
+		return
+	}
+	c.String(http.StatusOK, "verify,",claim.Username)
+}
+
+func refresh(c *gin.Context) {
+	strToken := c.Param("token")
+	claims,err := verifyAction(strToken)
+	if err != nil {
+		c.String(http.StatusNotFound, err.Error())
+		return
+	}
+	claims.ExpiresAt = time.Now().Unix() + (claims.ExpiresAt - claims.IssuedAt)
+	signedToken,err:=getToken(claims)
+	if err!=nil{
+		c.String(http.StatusNotFound, err.Error())
+		return
+	}
+	c.String(http.StatusOK, signedToken)
+}
+
+func verifyAction(strToken string) (*JWTClaims, error) {
+	token, err := jwt.ParseWithClaims(strToken, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(Secret), nil
+	})
+	if err != nil {
+		return nil, errors.New(ErrorReason_ServerBusy)
+	}
+	claims, ok := token.Claims.(*JWTClaims)
+	if !ok {
+		return nil, errors.New(ErrorReason_ReLogin)
+	}
+	if err := token.Claims.Valid(); err != nil {
+		return nil, errors.New(ErrorReason_ReLogin)
+	}
+	fmt.Println("verify")
+	return claims, nil
+}
+
+func getToken(claims *JWTClaims)(string,error){
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(Secret))
+	if err != nil {
+		return "",errors.New(ErrorReason_ServerBusy)
+	}
+	return signedToken,nil
 }
