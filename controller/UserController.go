d5f303f6246b446c484c9a764a9b282a1b52b662
nie <yesheng35@126.com>
2022-06-22 23:35:14 +0800

    asd

--- controller/UserController.go
+++ controller/UserController.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"layuiAdminstd/model"
 	"net/http"
+	"strconv"
 )
 
 func TestMysql(c *gin.Context) {
@@ -25,6 +26,69 @@ func CreateNewUser(c *gin.Context) {
 	result := model.CreateUser(user)
 	c.JSON(http.StatusOK, gin.H{
 		"data" : result,
+	})
+}
+
+// 查找用户
+func FindUserDetail(c *gin.Context) {
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.JSON(200, gin.H{
+			"data": "请上传正确的数据",
+		})
+		return
+	}
+	result := model.FindForId(id)
+	c.JSON(http.StatusOK, gin.H{
+		"data" : result,
+	})
+}
+
+// 编辑用户
+func UpdateUserInfo(c *gin.Context) {
+	// 查询
+	id, err := strconv.Atoi(c.PostForm("id"))
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"data":"请选择正确的数据",
+		})
+		return
+	}
+	result := model.FindForId(id)
+	if result.Id != id {
+		c.JSON(http.StatusOK, gin.H{
+			"data" : "用户不存在",
+		})
+		return
+	}
+	//sex, _ := strconv.Atoi(c.PostForm("sex"))
+	newInfo := map[string]interface{}{
+		"username" : c.PostForm("username"),
+	}
+	updateRes := model.UpdateUserForId(id, newInfo)
+	c.JSON(http.StatusOK, gin.H{
+		"data" : updateRes,
+	})
+}
+
+func DeleteUserInfo(c *gin.Context) {
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"data":"请选择正确的数据",
+		})
+		return
+	}
+	result := model.FindForId(id)
+	if result.Id != id {
+		c.JSON(http.StatusOK, gin.H{
+			"data":"用户不存在",
+		})
+		return
+	}
+	res := model.DeleteUser(id)
+	c.JSON(http.StatusOK, gin.H{
+		"data": res,
 	})
 }
 
--- model/user.go
+++ model/user.go
@@ -20,3 +20,23 @@ func CreateUser(user User) *gorm.DB {
 	result := GetDB().Table(TableName()).Create(&user)
 	return result
 }
+
+// 查找用户
+func FindForId(id int) *User {
+	user := User{}
+	GetDB().Table(TableName()).Where("uid = ?", id).Find(&user)
+	return &user
+}
+
+// 更新数据
+func UpdateUserForId(id int, data map[string]interface{}) * User {
+	user := User{}
+	GetDB().Table(TableName()).Model(&user).Where("uid = ?", id).Updates(data)
+	return &user
+}
+
+// 删除用户
+func DeleteUser(id int) *gorm.DB {
+	user := User{}
+	return GetDB().Table(TableName()).Delete(&user, id)
+}
