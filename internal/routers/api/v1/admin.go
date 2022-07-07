58e3734f47f0b6147c9bae2892a8afe33a24a7ab
Lucius <yesheng35@126.com>
2022-07-07 18:30:40 +0800

    添加错误编码

--- internal/routers/api/v1/admin.go
+++ internal/routers/api/v1/admin.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"layuiAdminstd/internal/service"
 	"layuiAdminstd/pkg/app"
+	"layuiAdminstd/pkg/convert"
 	"layuiAdminstd/pkg/errcode"
 )
 
@@ -46,7 +47,19 @@ func (a Admin) List(c *gin.Context) {
 	return
 }
 
-
+// @Summary 创建
+// @Produce json
+// @Param tag_id body string true "标签ID"
+// @Param title body string true "文章标题"
+// @Param desc body string false "文章简述"
+// @Param cover_image_url body string true "封面图片地址"
+// @Param content body string true "文章内容"
+// @Param created_by body int true "创建者"
+// @Param state body int false "状态"
+// @Success 200 {object} model.Article "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles [post]
 func (a Admin) Create(c *gin.Context) {
 	param := service.CreateAdminRequest{}
 	response := app.NewResponse(c)
@@ -59,7 +72,19 @@ func (a Admin) Create(c *gin.Context) {
 	}
 }
 
-func (a Admin) Update(c *gin.Context) {}
+func (a Admin) Update(c *gin.Context) {
+	param := service.UpdateAdminRequest{ID: convert.StrTo(c.Param("id")).MustUInt32()}
+	response := app.NewResponse(c)
+	svc := service.New(c.Request.Context())
+	err := svc.UpdateAdmin(&param)
+	if err != nil {
+		global.Logger.Errorf("svc.UpdateAdmin err: %v", err)
+		response.ToErrorResponse(errcode.ErrorUpdateAdminFail)
+		return
+	}
+	response.ToResponse(gin.H{})
+	return
+}
 
 func (a Admin) Delete(c *gin.Context) {}
 
--- pkg/errcode/module_code.go
+++ pkg/errcode/module_code.go
@@ -3,6 +3,11 @@ package errcode
 var(
 	ErrorGetAdminListFail = NewError(20010001, "获取列表失败")
 	ErrorCreateAdminFail = NewError(20010002, "新建失败")
+	ErrorUpdateAdminFail = NewError(20010003, "编辑失败")
+	ErrorDeleteAdminFail = NewError(20010004, "删除失败")
 
-	
+
+	ErrorUploadFileFail = NewError(20030001, "上传文件失败")
+
+
 )
