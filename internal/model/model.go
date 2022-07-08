00f89187348b113d98515aaf86ed74a422a98540
Lucius <yesheng35@126.com>
2022-07-08 10:13:35 +0800

    asd

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"fmt"
+	// 开放式跟踪器
 	otgorm "github.com/eddycjy/opentracing-gorm"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
@@ -25,6 +26,7 @@ type Model struct {
 	IsDel uint8
 }
 
+// 初始化数据库链接
 func NewDBEngine(databaseSetting *setting.DatabaseSettings) (*gorm.DB, error) {
 	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
 	db, err := gorm.Open(databaseSetting.DBType, fmt.Sprintf(s,
@@ -40,21 +42,28 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettings) (*gorm.DB, error) {
 		return nil, err
 	}
 
-	fmt.Sprintf("11111111111111111111111111111111111111111");
+	fmt.Sprintf("11111111111111111111111111111111111111111")
 
 	if global.ServerSetting.RunMode == "debug" {
 		db.LogMode(true)
 	}
-	//db.SingularTable(true)
-	//db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
-	//db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
-	//db.Callback().Delete().Replace("gorm:delete", deleteCallback)
+	db.SingularTable(true)
+	// 三个对应的回调已经写好并替换掉默认的钩子回调
+	// 注册 Callbacks
+	db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
+	db.Callback().Delete().Replace("gorm:delete", deleteCallback)
 	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
 	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
 	otgorm.AddGormCallbacks(db)
 	return db, nil
 }
-
+/**
+结合 GORM 完成了新增、更新、查询的 Callbacks，在实际项目中常常也是这么使用
+毕竟，一个钩子的事，就没有必要自己手写过多不必要的代码了
+（注意，增加了软删除后，先前的代码需要增加 deleted_on 的判断）
+ */
+// 注册回调，
 func updateTimeStampForCreateCallback(scope *gorm.Scope) {
 	if !scope.HasError() {
 		newTime := time.Now().Unix()
@@ -81,10 +90,12 @@ func updateTimeStampForUpdateCallback(scope *gorm.Scope ) {
 func deleteCallback(scope *gorm.Scope) {
 	if !scope.HasError() {
 		var extraOption string
+		// scope.Get("gorm:delete_option") 检查是否手动指定了 delete_option
 		if str, ok := scope.Get("gorm:delete_option"); ok {
 			extraOption = fmt.Sprint(str)
 		}
 
+		// scope.FieldByName("DeletedOn") 获取我们约定的删除字段，若存在则 UPDATE 软删除，若不存在则 DELETE 硬删除
 		deletedOnField, hasDeletedOnField := scope.FieldByName("DeletedOn")
 		isDelField, hasIsDelField := scope.FieldByName("IsDel")
 		if !scope.Search.Unscoped && hasDeletedOnField && hasIsDelField {
@@ -102,7 +113,9 @@ func deleteCallback(scope *gorm.Scope) {
 		}else {
 			scope.Raw(fmt.Sprintf(
 				"DELETE FROM %v%v%v",
+				// scope.QuotedTableName() 返回引用的表名，这个方法 GORM 会根据自身逻辑对表名进行一些处理
 				scope.QuotedTableName(),
+				// scope.CombinedConditionSql() 返回组合好的条件 SQL，看一下方法原型很明了
 				addExtraSpaceIfExist(scope.CombinedConditionSql()),
 				addExtraSpaceIfExist(extraOption),
 			)).Exec()
