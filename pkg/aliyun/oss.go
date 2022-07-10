256e84a9fe6555ab0acd95af941bb0f409cdb27b
nie <yesheng35@126.com>
2022-07-10 09:47:34 +0800

    asd

--- pkg/aliyun/oss.go
+++ pkg/aliyun/oss.go
@@ -43,5 +43,24 @@ func (that *OssServer) Init (accessKeyId, accessKeySecret, bucketName, endPoint 
 //	if err != nil {
 //		fmt.Println(err)
 //	}
+//
+//	// 字符串上传下载
+//	_ = bucket.PutObject("my-object-1", strings.NewReader("Hello Oss"))
+//	rd, _ := bucket.GetObject("my-object-1")
+//	data, _ := ioutil.ReadAll(rd)
+//	rd.Close()
+//	fmt.Println(string(data))
+//
+//	// 文件上传下载
 //	_ = bucket.PutObjectFromFile("a/b.txt", "/Users/go/Downloads/1.txt")
+//	_ = bucket.GetObjectToFile("my-object-2", "mynewpic.jpg")
+//
+//	// 分片并发，断电续传上传/下载
+//	_ = bucket.UploadFile("my-object-3", "mypic.jpg", 100*1024, oss.Routines(3), oss.Checkpoint(true, ""))
+//	_ = bucket.DownloadFile("my-object-3", "mynewpic.jpg", 100*1024, oss.Routines(3), oss.Checkpoint(true, ""))
+//
+//	// 查看object
+//	lsRes, _ := bucket.ListObjects()
+//	fmt.Println("my objects", lsRes.Objects)
+//
 //}
