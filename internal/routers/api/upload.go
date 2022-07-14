d0aeff3e3b1a3b3724bc515c6de416b30479f027
Lucius <yesheng35@126.com>
2022-07-14 11:35:45 +0800

    更新数据,添加文件上传下载

--- internal/routers/api/upload.go
+++ internal/routers/api/upload.go
@@ -9,6 +9,9 @@ import (
 	"layuiAdminstd/pkg/convert"
 	"layuiAdminstd/pkg/errcode"
 	"layuiAdminstd/pkg/upload"
+	"net/http"
+	"os"
+	"path"
 )
 
 type Upload struct {}
@@ -45,5 +48,31 @@ func (u Upload) UploadFile(c *gin.Context) {
 	response.ToResponse(gin.H{
 		"file_access_url":fileInfo.AccessUrl,
 	})
+}
+
+// 下载次数
+func fileDownload(c *gin.Context) {
+	filePath := c.Query("url")
+	// 打开文件
+	fileTmp, errByOpenFile := os.Open(filePath)
+	defer fileTmp.Close()
+
+	// 获取文件的名称
+	fileName := path.Base(filePath)
+	if errByOpenFile != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message":"文件获取失败",
+		})
+	}
+
+	c.Header("Content-Type", "application/octet-stream")
+	// 强制浏览器下载
+	c.Header("Content-Disposition", "attachment; filename=" + fileName)
+	// 浏览器下载或预览
+	c.Header("Content-Disposition", "inline; filename=" + fileName)
+	c.Header("Content-Transfer-Encoding", "binary")
+	c.Header("Cache-Control", "no-cache")
+	c.File(filePath)
+	return
 }
 
--- internal/service/upload.go
+++ internal/service/upload.go
@@ -1,10 +1,14 @@
 package service
 
 import (
+	"github.com/gin-gonic/gin"
+	"io"
 	"layuiAdminstd/global"
 	"layuiAdminstd/pkg/upload"
 	"mime/multipart"
+	"net/http"
 	"os"
+	"path"
 )
 
 type FileInfo struct {
@@ -40,4 +44,83 @@ func (svc *Service) UploadFile(fileType upload.FileType, file multipart.File, fi
 
 	accessUrl := global.AppSetting.UploadServerUrl + "/" + fileName
 	return &FileInfo{Name: fileName, AccessUrl:  accessUrl}, nil
+}
+
+
+func UploadResource(c *gin.Context) {
+	saveDirParam := c.PostForm("saveDir")
+	fileNameParam := c.PostForm("fileName")
+
+	var saveDir = ""
+	var saveName = ""
+	var savePath = ""
+	file, header, errFile := c.Request.FormFile("file")
+	if errFile != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": "请选择文件",
+			"dir": saveDir,
+			"name": saveName,
+			"path": savePath,
+		})
+		return
+	}
+
+	// 回去上传文件后缀(类型）
+	uploadFileNameWithSuffix := path.Base(header.Filename)
+	uploadFileType := path.Ext(uploadFileNameWithSuffix)
+	// 文件保存目录
+	saveDir = "" + saveDirParam
+	// 保存的文件名称
+	saveName = fileNameParam + uploadFileType
+	savePath = saveDir + "/" + saveName
+	// 打开目录
+	localFileInfo, fileStatErr := os.Stat(saveDir)
+	// 目录不存在
+	if fileStatErr != nil || !localFileInfo.IsDir() {
+		// 创建目录
+		errByMkdirAllDir := os.MkdirAll(saveDir, 0755)
+		if errByMkdirAllDir != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"dir": saveDir,
+				"name": saveName,
+				"path":savePath,
+				"message":"创建目录失败",
+			})
+			return
+		}
+	}
+	// 上传文件前，先删除该资源之前上传过的资源文件
+	// 编辑-重新选择文件，需要先删除资源之前上传过的资源文件
+	// 该代码执行的条件——上传的名称是唯一的，否则会出现误删
+	// 获取文件的前缀
+	// fileNameOnly := fileNameParam
+	// deleteFileWithName(fileNameOnly, saveDir)
+	// deleteFileWithName(fileNameOnly, model.WebConfig.ResourcePath + "/" + model.WebConfig.WebConvertToPath)
+
+	out, err := os.Create(savePath)
+	if err != nil {
+
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"dir": saveDir,
+			"name": saveName,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"dir": saveDir,
+		"name": saveName,
+		"path": savePath,
+		"message": "上传成功",
+	})
 }
